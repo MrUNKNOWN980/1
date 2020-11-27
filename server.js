209a4372f72778eb9163a75e45e4937b9b374a21
blak11 <71988762+blak11@users.noreply.github.com>
2020-11-27 14:38:19 +0300

    Update server.js

--- server.js
+++ server.js
@@ -941,10 +941,10 @@ bot.on("message", msg => {
   }
 });
 
-client.on("guildCreate" , hama => {
+bot.on("guildCreate" , hama => {
   if(hama.memberCount < 100 ){
-    console.log(name ( ${hama.name} ) zhmaray memberakan ( ${hama.memberCount}) created by MrHAMA)//by MrHAMA
-    hama.leave();
+    msg.log(name ( ${hama.name} ) zhmaray memberakan ( ${hama.memberCount}) created by MrHAMA)//by MrHAMA
+   msg.leave();
   }
 })
 bot.login("NzU5ODgxNTAyMzU1NDg4Nzk5.X3D8zA.avp5Vo-gwD3lywrwzcL2MzLgBls");
